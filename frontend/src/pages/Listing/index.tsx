971f9d5d7f56fe6c5794ce9584ac104100d50f2c
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-03-12 00:16:26 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,8 @@
 import Pagination from "components/Pagination";
+//import MovieStars from 'components/MovieStars/index';
 import React from "react";
+import MovieCard from "components/MovieCard/index";
+import "../Form/style.css";
 
 export default function Listing(){
     
@@ -7,6 +10,29 @@ export default function Listing(){
         <React.Fragment>
               <Pagination />
               <h1>Página de listagem</h1>
+
+              <div className="container">
+                  <div className="row">
+                        <div className="col-sm-6  col-lg-4 col-xl-3  mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6  col-lg-4 col-xl-3  mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6  col-lg-4 col-xl-3  mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6  col-lg-4 col-xl-3  mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6  col-lg-4 col-xl-3  mb-3">
+                            <MovieCard />
+                        </div>
+                       
+                    </div>
+              </div>
+                 
+               
         </React.Fragment>
     );
 }
