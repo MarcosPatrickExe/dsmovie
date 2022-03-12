ab81bb8e9757905fbf0d3be88d46936410daa606
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-03-11 21:59:07 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,4 +1,12 @@
+import Pagination from "components/Pagination";
+import React from "react";
+
 export default function Listing(){
     
-    return <h1>Página de listagem</h1>
+    return (
+        <React.Fragment>
+              <Pagination />
+              <h1>Página de listagem</h1>
+        </React.Fragment>
+    );
 }
