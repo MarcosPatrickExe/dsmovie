ff3b0bb4b0c7f717011876a9758c1ca196df5658
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-03-16 10:21:30 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,31 @@
 import Pagination from "components/Pagination";
-//import MovieStars from 'components/MovieStars/index';
+import axios from "axios";
+//import {BASE_URL} from "utils/requests";
 import React from "react";
 import MovieCard from "components/MovieCard/index";
 import "../Form/style.css";
 
 export default function Listing(){
-    
+
+    const backend_domain = "https://patrick-dsmovie-backend.herokuapp.com";
+
+    var config = {
+        headers: {'Access-Control-Allow-Origin':'*'}
+    }
+
+  //  axios.get( `${BASE_URL}/movies?size=12&page=0`)
+    axios.get( `${backend_domain}/movies?size=12&page=0`, config)
+
+        .then( (response : any) =>{
+
+            console.log(typeof(response));
+            console.log(response.data);
+
+        }).catch( (error: any)=>{
+            alert("Erro ao tentar acessar o servidor backend....");
+        });
+
+
     return (
         <React.Fragment>
               <Pagination />
